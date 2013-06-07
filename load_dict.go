8db2285ab987f9e7818cc648ac03dc57be7dc767
MURAOKA Taro <taro.muraoka@muraoka-design.com>
2013-06-07 17:07:34 +0900

    measure time to load

--- load_dict.go
+++ load_dict.go
@@ -6,26 +6,29 @@ import (
     "flag"
     "fmt"
     "strings"
+    "time"
 )
 
-func LoadDict() {
+func LoadDict(path string) {
+    trie := trie.NewTernaryTrie()
+    count := 0
+    start := time.Now()
+    for line := range fileutil.NewLineReadChannel(path) {
+        trie.Add(strings.TrimSpace(line), count)
+        count++
+    }
+    elapsed := time.Since(start)
+    fmt.Printf("elapsed=%f\n", elapsed.Seconds())
+    //fmt.Printf("trie.Count=%d\n", trie.Count())
+    //fmt.Printf("trie.Find(\"あ\")=%s\n", trie.Find("あ"))
+}
+
+func main() {
     flag.Parse()
     if flag.NArg() < 1 {
         fmt.Println("Too few arguments");
         return
     }
     path := flag.Arg(0)
-
-    trie := trie.NewTernaryTrie()
-    count := 0
-    for line := range fileutil.NewLineReadChannel(path) {
-        trie.Add(strings.TrimSpace(line), count)
-        count++
-    }
-    fmt.Printf("trie.Count=%d\n", trie.Count())
-    fmt.Printf("trie.Find(\"あ\")=%s\n", trie.Find("あ"))
-}
-
-func main() {
-    LoadDict()
+    LoadDict(path)
 }
